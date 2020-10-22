556f30b6d904137248bda10a70e4afd40399a95a
Gabrielle Jones <jonesg22@mail.wlu.edu>
2020-10-22 13:18:01 -0400

    Added regression lines, emphasis on lines (plural)

--- exchange-programs-vs-hiv-scatterplot.r
+++ exchange-programs-vs-hiv-scatterplot.r
@@ -2,7 +2,8 @@ library(ggplot2)
 
 nep_vs_hiv_table <- read.table("num-needle-exchange-programs.txt", header = TRUE, sep = ",")
 
-nep_vs_hiv_plot <- ggplot(nep_vs_hiv_table, aes(Number.of.Needle.Exchange.Programs, New.HIV.Cases.per.100000.People, color = Region)) + geom_point()
+print(ggplot(nep_vs_hiv_table, aes(Number.of.Needle.Exchange.Programs, New.HIV.Cases.per.100000.People, color = Region)) + geom_point() + geom_smooth(method = lm, se = FALSE))
+
 
 
 
