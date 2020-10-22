9611994b7dc1d8083a5339081657e78bcee07971
Gabrielle Jones <jonesg22@mail.wlu.edu>
2020-10-22 12:53:29 -0400

    Changed text file such that ggplot would have meaningful titles

--- exchange-programs-vs-hiv-scatterplot.r
+++ exchange-programs-vs-hiv-scatterplot.r
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-nep_vs_hiv <- read.table("num-needle-exchange-programs.txt", header = TRUE)
+nep_vs_hiv <- read.table("num-needle-exchange-programs.txt", header = TRUE, sep = ",")
 
-print(ggplot(nep_vs_hiv, aes(NEP, HIV, color = Region)) + geom_point())
+print(ggplot(nep_vs_hiv, aes(Number.of.Needle.Exchange.Programs, New.HIV.Cases.per.100000.People, color = Region)) + geom_point())
 
 
 
