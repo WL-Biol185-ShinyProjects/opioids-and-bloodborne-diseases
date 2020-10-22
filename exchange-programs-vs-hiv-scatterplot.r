742057b26870a495bdc704419937c67ddc06a1a5
Gabrielle Jones <jonesg22@mail.wlu.edu>
2020-10-22 13:02:24 -0400

    Created variable for scatterplot

--- exchange-programs-vs-hiv-scatterplot.r
+++ exchange-programs-vs-hiv-scatterplot.r
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-nep_vs_hiv <- read.table("num-needle-exchange-programs.txt", header = TRUE, sep = ",")
+nep_vs_hiv_table <- read.table("num-needle-exchange-programs.txt", header = TRUE, sep = ",")
 
-print(ggplot(nep_vs_hiv, aes(Number.of.Needle.Exchange.Programs, New.HIV.Cases.per.100000.People, color = Region)) + geom_point())
+nep_vs_hiv_plot <- ggplot(nep_vs_hiv_table, aes(Number.of.Needle.Exchange.Programs, New.HIV.Cases.per.100000.People, color = Region)) + geom_point()
 
 
 
