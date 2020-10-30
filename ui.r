916157ea15018144de82a9a37b74b4172ab8cd56
Gabrielle Jones <jonesg22@mail.wlu.edu>
2020-10-30 19:08:15 -0400

    Added introduction text

--- ui.r
+++ ui.r
@@ -5,6 +5,12 @@ library(ggplot2)
 #Application Title
 navbarPage(title = span( "Opioids and HIV", style = "background-color: #AAA1C8"),
 
+           #Introduction to Our Project
+           navbarMenu("Introduction",
+                      tabPanel("The Opioid Epidemic and HIV",
+                               p("Due to the overprescription of opioid medications by doctors beginning in the 1990s, rates of opioid abuse and overdose increased over the subsequent decades. Many people originally prescribed opioid painkillers eventually turned to heroin and synthetic opioids, which are generally cheaper than prescriptions. With the use of intravaneous drugs comes the risk of bloodborne diseases, like HIV. Our project seeks to describe the correlation between opioid overprescription, opioid abuse, and rates of HIV infection."))),
+           
+           
            #Maps - needs to have multiple tabs
           navbarMenu("Maps",
             tabPanel("Opioid Mortality by State",
